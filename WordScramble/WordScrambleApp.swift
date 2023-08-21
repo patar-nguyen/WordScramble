0a7b139ca834c7fbc7e9776fb7cf291d56f55c25
Patrick Nguyen <patar.nguyen@gmail.com>
2023-08-21 15:15:38 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Patrick Nguyen on 2023-08-21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
